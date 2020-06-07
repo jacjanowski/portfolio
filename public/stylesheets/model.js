0176a04661a050dbbb27fa30bb440ed259ae3b91
jacjanowski <jacobjanowskiedu@yahoo.com>
2020-06-07 21:29:17 +0000

    add scroll effect to css. only applies to one element with '#fade-scroll.

--- public/stylesheets/model.js
+++ public/stylesheets/model.js
@@ -1,6 +1,20 @@
-		$(document).ready(function () {
+$(document).ready(function () {
 
-			$("#pop").click(function(){
-				 $('#popper').modal('show');
-			});
-		});
+	$("#modalBtn").click(function(){
+		 $('#modalShow').modal('show');
+	});
+});
+
+$(document).ready(function() {
+    $('a').click(function() {
+        $('.item.active').removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+$(window).scroll(function(){
+	$("#fade-scroll").each(function(){
+		$("#fade-scroll").css("opacity", 1 - $(window).scrollTop() / 350);
+	});
+    
+  });
