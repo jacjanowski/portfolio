a590321f6e7effe74ae5c10556ca96dc0a56fd4e
jacjanowski <jacobjanowskiedu@yahoo.com>
2020-06-13 19:48:18 +0000

    jumbotron fix

--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,22 @@ var mongoose = require("mongoose");
  
 var profileSchema = new mongoose.Schema({
    name: String,
-   avitar: String,
-   summary: String,
-
+   image: String,
+   price: String,
+   description: String,
+   author: {
+	   id: {
+		   type: mongoose.Schema.Types.ObjectId,
+		   ref: "User"
+	   },
+	   username: String
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
 });
  
 module.exports = mongoose.model("Profile", profileSchema);
