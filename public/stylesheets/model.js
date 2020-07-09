879585988d4589557a3a9b3636b954cd201f6a72
jacjanowski <jacobjanowskiedu@yahoo.com>
2020-07-09 03:48:59 +0000

    fade scroll done

--- public/stylesheets/model.js
+++ public/stylesheets/model.js
@@ -19,51 +19,19 @@ $(window).scroll(function(){
     
   });
 
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         $("#hero-image").css("opacity", 1 - $(window).scrollTop() / $('#hero-image').height());
-//     });
-// });
 
-// $(window).on("load",function() {
-//   $(window).scroll(function() {
-//     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-//     $(".fade").each(function() {
-//       /* Check the location of each desired element */
-//       var objectBottom = $(this).offset().top + $(this).outerHeight();
-      
-//       /* If the element is completely within bounds of the window, fade it in */
-//       if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-//         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-//       } else { //object goes out of view (scrolling up)
-//         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-//       }
-//     });
-//   }).scroll(); //invoke scroll-handler on page-load
-// });
+$(document).on('scroll', function () {
+	var pageTop = $(document).scrollTop();
+	var pageBottom = pageTop + $(window).height();
+	var tags = $('section');
 
+	for (var i = 0; i < tags.length; i++) {
+		var tag = tags[i];
 
-$(window).on("load", function () {
-    function fade() {
-        $('.fade').each(function () {
-            /* Check the location of each desired element */
-            var objectBottom = $(this).offset().top + $(this).outerHeight();
-            var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-
-            /* If the object is completely visible in the window, fade it in */
-            if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-                if ($(this).css('opacity') != 1) {
-                    $(this).stop(true,false).fadeTo(500, 1);
-                }
-            } else { //object goes out of view (scrolling up)
-                if ($(this).css('opacity') == 1) {
-                    $(this).stop(true,false).fadeTo(500, .3);
-                }
-            }
-        });
-    }
-    fade(); //Fade in completely visible elements during page-load
-    $(window).scroll(function () {
-        fade();
-    }); //Fade in elements during scroll
+		if ($(tag).position().top < pageBottom) {
+			$(tag).addClass('visible');
+		} else {
+			$(tag).removeClass('visible');
+		}
+	}
 });
