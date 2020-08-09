a773e1b02a9ba314c878b7315908910a45e57b0f
jacjanowski <jacobjanowskiedu@yahoo.com>
2020-08-09 05:16:50 +0000

    insert date format on contact page.

--- app.js
+++ app.js
@@ -23,12 +23,24 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + '/public'));
 
+
+// current timestamp in milliseconds
+let ts = Date.now();
+
+let date_ob = new Date(ts);
+let day = date_ob.getDate();
+let month = date_ob.getMonth() + 1;
+let year = date_ob.getFullYear();
+let date = month + "/" + day + "/" + year;
+
+
+
 app.get("/", function(req,res){
 	res.render("landing");
 });
 
 app.get("/contact", function(req,res){
-	res.render("contact");
+	res.render("contact", {date: date} );
 });
 
 app.listen(PORT,function(){
