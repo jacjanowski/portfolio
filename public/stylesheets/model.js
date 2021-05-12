5415c5be2544114ac53b654649c1538175af8ae1
jacjanowski <jacobjanowskiedu@yahoo.com>
2021-05-12 19:40:43 +0000

    add code for horiztontal slider

--- public/stylesheets/model.js
+++ public/stylesheets/model.js
@@ -19,6 +19,28 @@ $(window).scroll(function(){
     
   });
 
+$(document).ready(function() {
+  $('.logo-carousel').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: true,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 2
+      }
+    }]
+  });
+});
 
 $(document).on('scroll', function () {
 	var pageTop = $(document).scrollTop();
